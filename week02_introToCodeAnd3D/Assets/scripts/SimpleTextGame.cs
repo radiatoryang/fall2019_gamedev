4d95e64c7492c16e88d968e95bc3ccc7a424202a
Robert Yang <yang.robert.w@gmail.com>
2019-09-11 17:59:37 -0400

    added week02b: chair models, and SimpleTextGame.cs

---
+++ week02_introToCodeAnd3D/Assets/scripts/SimpleTextGame.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // you need this to talk to UI components
+
+// usage: put this on a Text GameObject
+// intent: player must hold SPACE for 10 seconds to win; display text timer
+public class SimpleTextGame : MonoBehaviour
+{
+    Text myText;
+    float myScore = 0f;
+
+    void Start()
+    {
+        myText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // display current score?
+        myText.text = "SUPER FUN GAME:\nhold SPACE for 10 seconds to win\n" + myScore.ToString();
+
+        // is user holding down space? then add to score
+        if ( Input.GetKey( KeyCode.Space ) ) {
+            myScore += Time.deltaTime; // dT = the duration of the frame in seconds
+            // 10 FPS... dT = 0.1f
+            // 100 FPS... dT = 0.01f;
+            // faster FPS = smaller deltaTime, slower FPS = bigger deltaTime
+            // multiply values by dT to make it FRAMERATE INDEPENDENT
+        }
+
+        // did user get within 0.1 of 10.0?
+        if ( Mathf.Abs(10f - myScore) < 0.1f ) {
+            myText.text += "\nGOOD ENOUGH";
+        }
+
+        // add a cheat code where we can press K to get exactly 10
+        if ( Input.GetKeyDown(KeyCode.K) ) {
+            myScore = 10f;
+        }
+    }
+}
